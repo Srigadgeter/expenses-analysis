3dc5aaab9d42875fd930d867b663fa61a8818fd0
Srigadgeter <sridhar.t@ezedox.com>
2019-07-18 20:34:15 +0530

    routes creation

---
+++ src/routes/Routes.js
@@ -0,0 +1,32 @@
+import React, { Suspense } from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import { routes } from './RouteDatas';
+
+const history = createBrowserHistory();
+
+const RouteCreator = (route) => (
+    <Route 
+        key = {route.path}
+        path = {route.path}
+        exact = {route.path === '/'}
+        render = {() => (<route.component />)}
+    />
+)
+
+const Routes = () => {
+    let routeDatas = Object.values(routes);
+
+    return (
+        <Router history={history}>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Switch>
+                    { routeDatas.map(route => ( RouteCreator(route) )) }
+                </Switch>
+            </Suspense>
+        </Router>
+    )
+}
+
+export default Routes;
