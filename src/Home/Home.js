3e449463e331701a1ca8304c85602f3080d1028a
Srigadgeter <sridhar.t@ezedox.com>
2019-07-20 02:03:16 +0530

    Home Page Created with css & routed

---
+++ src/Home/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import { routes } from '../routes/RouteDatas';
+import './Home.css';
+
+class Home extends Component {
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push(routes.DASHBOARD.path);
+        }, 4000)
+    }
+
+    render() {
+        return (
+            <div className="home" style={{width: window.innerWidth, height: window.innerHeight}}>
+                <span className="project-title font-mona">EXPENSES ANALYSIS</span>
+            </div>
+        )
+    }
+}
+
+export default withRouter(Home);
